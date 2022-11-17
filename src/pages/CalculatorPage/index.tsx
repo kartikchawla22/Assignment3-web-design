f76647a3c081e9b69a8f84875f45a3da6e83d23d
Kartik Chawla <kchawla1995@gmail.com>
2022-11-17 03:14:30 -0500

    heaidng for calc added

--- src/pages/CalculatorPage/index.tsx
+++ src/pages/CalculatorPage/index.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Calculator from '../../components/calculator';
+import styles from './index.module.scss';
 const CalculatorPage = () => {
     return (
-        <Calculator />
+        <div>
+            <Typography variant="h3" noWrap component="div" className={styles.heading}>
+                Calculator
+            </Typography>
+            <Calculator />
+        </div>
     )
 }
 export default CalculatorPage
